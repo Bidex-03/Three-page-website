79f9df5cbe15e5f99f4a212b3f5d260db11c5c6a
Bidex-03 <abdullahqaasim14@gmail.com>
2023-05-27 23:06:35 +0100

    feat: adjusted the homepage texts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,14 @@ const HomePage = () => {
   return (
     <Card className="w-[80%] md:w-[50%] lg:w-[30%] mx-auto mt-4 flex justify-center">
       <div className="p-4 text-justify">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates
-        libero ab reiciendis eos sed recusandae aspernatur repellat numquam
-        saepe alias, corrupti optio voluptas. Inventore a cumque quo sit quod
-        numquam praesentium aspernatur accusantium delectus facere alias
-        voluptatum, aliquid molestias magni sapiente exercitationem. Perferendis
-        beatae nihil provident dignissimos quia! Consequatur et nisi perferendis
-        excepturi numquam pariatur, illo ullam? Consectetur, suscipit id.
-        Eligendi aliquam, quod natus similique necessitatibus suscipit minus
-        consequatur aperiam quidem fugit totam nihil odit modi earum, voluptatum
-        ratione sapiente nam fugiat inventore perferendis consequuntur dolores
-        laborum vel eveniet! At excepturi blanditiis sunt ullam alias ipsa,
-        cupiditate neque odit quas.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum vitae
+        rerum enim eligendi voluptas quod fugiat, similique aliquam magnam iste?
+        Quo nostrum commodi incidunt, officiis voluptates beatae soluta eius
+        iusto. Possimus rem a molestias nemo eveniet maxime deleniti? Ex quaerat
+        officia ipsa necessitatibus tempora tenetur dignissimos unde! Explicabo
+        odit ipsam, vel consequatur esse obcaecati fugiat rerum laudantium quasi
+        a consequuntur, ullam ea at facere, adipisci ratione labore animi illum
+        iusto.
       </div>
     </Card>
   );
